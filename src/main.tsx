7477157f8979c9518a7b964da3e3579e6edf79b0
nusaardiw <nusaardhy@gmail.com>
2026-01-19 11:09:57 +0700

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -6,4 +6,16 @@ import '@fontsource/cormorant-garamond/700.css';
 import App from "./App.tsx";
 import "./index.css";
 
+// Initialize Facebook Pixel if ID is present
+const PIXEL_ID = import.meta.env.VITE_FACEBOOK_PIXEL_ID;
+if (PIXEL_ID && typeof window !== 'undefined') {
+    // @ts-ignore
+    if (window.fbq) {
+        // @ts-ignore
+        window.fbq('init', PIXEL_ID);
+        // @ts-ignore
+        window.fbq('track', 'PageView');
+    }
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
